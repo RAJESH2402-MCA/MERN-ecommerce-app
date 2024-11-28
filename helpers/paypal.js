6b81b42e6b12dc0d406c3e57ad62a5881996978a
RAJESH2402-MCA <rajesh62309@gmail.com>
2024-11-28 13:02:00 +0530

    Delete helpers directory

--- helpers/paypal.js
+++
@@ -1,9 +0,0 @@
-const paypal = require('paypal-rest-sdk');
-
-paypal.configure({
-    mode : 'sandbox',
-    client_id : "AfhfuURCuHlF9m3GiWbv_4cmu_PkBckepXAXfq5m8LKn-ykkEp3n9Az7THooztBxSadquH20RlNYC3Ft",
-    client_secret: "EBXcQ5lo18rZKhZNKO8FOUM5tvqX8jW0yYLlygszIakqbYmEIatwyg8gXt-Oju4eI09xKU6lAQ4bks3e",
-})
-
-module.exports = paypal;
